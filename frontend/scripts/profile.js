740a6731bfbcb89466f8501d7892c5c45c4b054a
jainam0210 <jainammodi06@gmail.com>
2025-04-16 15:56:21 +0530

    1st commit profile.js

--- frontend/scripts/profile.js
+++ frontend/scripts/profile.js
@@ -3,6 +3,7 @@ async function loadUsers() {
   const res = await fetch(`/users`);
   const users = await res.json();
   const list = document.getElementById("userList");
+  const basURL = "http://localhost:8000"; // profile.js baseURL is undefined
   list.innerHTML = "";
   
   document.getElementById("userCounts").textContent = `Total users: ${users.length}`;
