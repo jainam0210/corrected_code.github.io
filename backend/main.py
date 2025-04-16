df074f7cb3cd29fab35703518804078c0d9b4d2c
Kavish Vora <kavishvora1808@gmail.com>
2025-04-16 15:39:02 +0530

    Changes in main.py 5

--- backend/main.py
+++ backend/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 app.include_router(items_router, prefix="/items")
 app.include_router(users_router, prefix="/users") # Added router
 app.include_router(analytics_router, prefix="/analytics") # Added prefix
-app.include_router(quiz_router)
+app.include_router(quiz_router, prefix="/quiz") # Added prefix
 
 # why the hell did I write this function?
 @app.get("/") # Changed route from /home to /
