aad585d45d93af80e5fc9594673971a617c135db
Kavish Vora <kavishvora1808@gmail.com>
2025-04-16 15:42:15 +0530

    Changes in items.py 2

--- backend/routes/items.py
+++ backend/routes/items.py
@@ -23,9 +23,9 @@ async def create_item(item: Item):
     result = await collection.insert_one(item.dict())
     return {"id": str(result.inserted_id)}
 
-@router.post("/")
-async def create_item(item: Item):
-    return {"id": "Item Inserted"}
+# @router.post("/")
+# async def create_item(item: Item):
+#     return {"id": "Item Inserted"} #Duplicate of post removed
 # I want a chocolate
 @router.delete("/{item_id}/{item_details}")
 async def delete_item(item_id: str, item_details:str):
