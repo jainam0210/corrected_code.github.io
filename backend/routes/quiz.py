118ca5b83b650d93b81dbb3bf1881f7fc1d90d55
Krypton711 <karthik.uni.2006@gmail.com>
2025-04-16 15:49:25 +0530

    made change in quiz.py

--- backend/routes/quiz.py
+++ backend/routes/quiz.py
@@ -48,7 +48,7 @@ async def get_question():
         "options": question["options"]
     }
 
-@router.get("/answer")
+@router.post("/answer") # get doesn't have a body changed to post
 async def submit_answer(data: dict):
     question_id = data.get("id")
     answer = data.get("answer")
