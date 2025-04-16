ae2d9f2cd78339610856c14c2e73d6f1cbe52024
Kavish Vora <kavishvora1808@gmail.com>
2025-04-16 15:38:39 +0530

    Changes in main.py 4

--- backend/main.py
+++ backend/main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 
 app.include_router(items_router, prefix="/items")
 app.include_router(users_router, prefix="/users") # Added router
-app.include_router(analytics_router)
+app.include_router(analytics_router, prefix="/analytics") # Added prefix
 app.include_router(quiz_router)
 
 # why the hell did I write this function?
